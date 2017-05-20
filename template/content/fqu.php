ae5bf468910fe63a01b8fbc20b3f44e99d538c45
ber34 <ber34@o2.pl>
2017-05-20 12:37:53 +0200

    Create fqu.php

---
+++ template/content/fqu.php
@@ -0,0 +1,17 @@
+<?php
+?>
+<div>
+<span><h3><b>Auto </b> - Uruchomienie Auto .</h3> </span>
+
+<span><h3><b>Banner</b>-  Zarabiania z Banner . </h3></span>
+
+<span><h3> Facebook  - Zarabiania z Facebook.</h3></span>
+
+<span><h3> Google+ - Zarabiania z Google+.</h3> </span>
+
+<span><h3> eBay - Zarabiania z eBay.</h3></span>
+
+<span><h3> Allegro - Zarabiania z Allegro.</h3> </span>
+
+<span><b>Profil</b> – Profil użytkownika .</span><br /><br />
+</div> 
