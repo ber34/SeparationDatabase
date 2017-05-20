1c936692f228c3663e5455cfc33873889ff00ca7
ber34 <ber34@o2.pl>
2017-05-20 12:35:41 +0200

    Create politykaprywatnosci.php

---
+++ template/content/politykaprywatnosci.php
@@ -0,0 +1,14 @@
+<center><table width="90%" height="100%" bordercolor="#000000" border="5"><tbody><tr><td>
+  <div align="left">
+  <ul>
+  <font color="red"><h1><b>Polityka Prywatności</b></h1></font><h2><b>FigureMan</b></h2>
+  
+ <li><font color="red"><h3><b>1. </b></font>Korzystając z Serwisu <b> FigureMan</b></h3></li>
+
+    <li><font color="red"><h4><b>2.</b></font> Nie wolno dodawać . <br /><br /></h4></li>
+
+   <li><font color="red"><h4><b>3.</b></font> Każdy użytkownik, może mieć tylko jedno konto. <br /><br /></h4></li>
+
+   <li><font color="red"><h4><b> 4.</b></font> Administrator może usunąć stronę bez podania przyczyny ! <br /><br /></h4></li>
+	</ul></div>                            
+</td></tr></tbody></table></center>
