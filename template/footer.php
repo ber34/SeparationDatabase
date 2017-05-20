b2f6bcf08a7b34298413b7a47daa389d4aec5ed2
ber34 <ber34@o2.pl>
2017-05-20 12:21:24 +0200

    Create footer.php

---
+++ template/footer.php
@@ -0,0 +1,11 @@
+<?php
+///$czas_stop = (float)microtime();
+////$czas1 = $czas_stop-$czas_start;
+////$czas2 = round($czas1, 3);
+//// <mark style="margin: 0 0 0.01% 33.0%;">Strona została wygenerowana w <?php //echo  $czas2; sek.</mark>
+?>
+   <aside>
+    <a style="color:#fff;" href="http://figureman.com" title="figureman" target="_blank" >Figure Man</a>
+   </aside>
+
+ <aside style="color:#fff;">Copyright © 2016 - <?php echo date("Y");?> r. All Rights Reserved.</aside>
