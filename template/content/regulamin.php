bf3a8ffa91ed41986900f22b2068b5d640e9a29c
ber34 <ber34@o2.pl>
2017-05-20 12:34:27 +0200

    Create regulamin.php

---
+++ template/content/regulamin.php
@@ -0,0 +1,19 @@
+<center><table width="90%" height="100%" bordercolor="#000000" border="5"><tbody><tr><td>
+  <div align="left">
+  <ul>
+  <font color="red"><h1><b>Regulamin</b></h1></font><h2><b>FigureMan</b></h2>
+  
+ <li><font color="red"><h4><b>1. </b></font>Korzystając z Serwisu <b> FigureMan</b>
+   użytkownik ma obowiązek zapoznać się z treścią poniższego regulaminu, <br />
+   oraz akceptuje jego postanowienia i zobowiązuje się do ich przestrzegania. <br /><br /></h4></li>
+
+    <li><font color="red"><h4><b>2.</b></font> Nie wolno dodawać . <br /><br /></h4></li>
+
+   <li><font color="red"><h4><b>3.</b></font> Każdy użytkownik, może mieć tylko jedno konto. <br /><br /></h4></li>
+
+   <li><font color="red"><h4><b> 4.</b></font> Administrator może usunąć stronę bez podania przyczyny ! <br /><br /></h4></li>
+
+  <li><font color="red"><h4><b> 5.</b></font> Administrator może dokonać zmian wprowadzonych przez Ciebie informacji,<br />
+   tak, by były poprawne i zgodne z wymogami FigureMan<br /><br /></h4></li>
+	</ul></div>                            
+</td></tr></tbody></table></center>
